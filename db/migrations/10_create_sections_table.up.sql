158c8fca11f7258d58a20709c472a52fac5de477
shuk <javierjg0709@gmail.com>
2026-05-15 11:05:38 -0500

    Modify sections.room_id to not have a hard FK on rooms table

--- db/migrations/10_create_sections_table.up.sql
+++ db/migrations/10_create_sections_table.up.sql
@@ -2,6 +2,6 @@ CREATE TABLE sections
 (
     id       SERIAL PRIMARY KEY,
     round_id SERIAL REFERENCES rounds (id),
-    room_id  INTEGER REFERENCES rooms (id),
+    room_id  INTEGER, -- unfortunately, sections can be associated with a room that has since been deleted.
     flight   INT NOT NULL
 );
