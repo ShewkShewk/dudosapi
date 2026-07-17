49057222a9ec4db4cbbf0537915c0891fa85da75
shuk <javierjg0709@gmail.com>
2026-07-17 18:55:21 -0500

    Remove foreign key constrains between speaker_awards -> student_id

--- db/migrations/16_add_speaker_awards_table.up.sql
+++ db/migrations/16_add_speaker_awards_table.up.sql
@@ -3,6 +3,6 @@ CREATE TABLE speaker_awards
     tournament_id SERIAL REFERENCES tournaments (id) ON DELETE CASCADE,
     event_id      SERIAL REFERENCES events (id) ON DELETE CASCADE,
     rank          INT NOT NULL,
-    student_id    SERIAL REFERENCES students (id) ON DELETE CASCADE,
+    student_id    SERIAL,
     PRIMARY KEY (tournament_id, event_id, rank)
 );
